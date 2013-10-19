b6707162e9d8d785ce3fa752aa0a6f6f3c974b19
matthew.odette <matthew@odiemedia.com>
2013-10-19 17:07:46 -0400

    First commit

---
+++ app/app/core/AMD.js
@@ -0,0 +1,72 @@
+/*global require*/
+'use strict';
+
+require.config({
+	baseUrl: 'app',
+	paths: {
+		jquery     :  ['https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
+				       '../lib/jquery.min.1.10.1'],
+
+		angular    : ['https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular.min',
+				      '../lib/angular.min.1.2.0-rc.2'],
+		ngRoute    : ['https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular-route.min',
+				      '../lib/angular-route.min.1.2.0-rc.2'],
+		ngCookies  : ['https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular-cookies.min',
+				      '../lib/angular-cookies.min.1.2.0-rc.2'],  
+		ngAnimate  : ['https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular-animate.min',
+				      '../lib/angular-animate.min.1.2.0-rc.2'], 
+		ngSanitize : ['https://ajax.googleapis.com/ajax/libs/angularjs/1.2.0-rc.2/angular-sanitize.min',
+					  '../lib/angular-sanitize.min.1.2.0-rc.2'],
+
+        // From http://angular-ui.github.io/bootstrap/ 
+        // --Boostrap 3 port is still incomplete
+		ngBootstrap : '../lib/angular-ui',
+
+		app: 'core/app',
+	},
+	shim: {
+		jquery: {
+			exports: '$'
+		},
+		angular: {
+			exports: 'angular',
+		},
+		ngRoute: {
+			exports: 'ngRoute',
+			deps: ['angular']
+		},
+		ngCookies: {
+			exports: 'ngCookies',
+			deps: ['angular']
+		},
+		ngAnimate: {
+			exports: 'ngAnimate',
+			deps: ['angular']
+		},
+		ngSanitize: {
+			exports: 'ngSanitize',
+			deps: ['angular']
+		},
+		ngBootstrap: {
+			exports: 'ngBootstrap',
+			deps: ['angular']
+		}
+	}
+});
+
+require(
+	[
+	// Dependencies from lib
+		'angular', 
+		'ngRoute',
+		'../lib/less.min.1.3.3',  
+	// Angular directives/controllers/services
+		'app',
+		'core/viewHomeController',
+		'core/commonRoutes',
+		'core/header'
+	], 
+	function (angular) {
+		angular.bootstrap(document, ['TheApp']);
+	}
+);
---
+++ app/app/core/app.js
@@ -0,0 +1,27 @@
+/*global define*/
+'use strict';
+
+// Hook up all modules to the 'app' module
+// Returns TheApp (see commonRoutes.js for usage)
+define(['angular', 'ngRoute', 'ngSanitize', 'ngBootstrap'], function (angular, ngRoute, ngSanitize, ngBootstrap) {
+
+	angular.module('HashBangURLs', ['ngRoute']).config(['$locationProvider', function($location) {
+	   	$location.hashPrefix('!');
+	}]);
+
+	angular.module('HTML5ModeURLs', ['ngRoute']).config(['$locationProvider', function($location) {
+	   	$location.html5Mode(true);
+	}]);
+
+	// Choose to inject either HashBangs or HTML5 mode, your preference.
+	var TheApp = angular.module('TheApp', ['HashBangURLs', 'ui.bootstrap', 'ngSanitize'], function ($routeProvider, $locationProvider, $httpProvider) {
+
+	});
+
+	return TheApp;
+
+});
+
+
+
+
