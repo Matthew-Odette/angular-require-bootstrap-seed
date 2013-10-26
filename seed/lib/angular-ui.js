93c2584ade1c64bbc2939342a438293d60d58cbb
matthew.odette <matthew@odiemedia.com>
2013-10-26 13:49:51 -0400

    ngBootstrap: update alert to bootstrap 3.0

--- seed/lib/angular-ui.js
+++ seed/lib/angular-ui.js
@@ -1,7 +1,7 @@
 //angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.transition","ui.bootstrap.collapse","ui.bootstrap.accordion","ui.bootstrap.alert","ui.bootstrap.bindHtml","ui.bootstrap.buttons","ui.bootstrap.carousel","ui.bootstrap.position","ui.bootstrap.datepicker","ui.bootstrap.dropdownToggle","ui.bootstrap.modal","ui.bootstrap.pagination","ui.bootstrap.tooltip","ui.bootstrap.popover","ui.bootstrap.progressbar","ui.bootstrap.rating","ui.bootstrap.tabs","ui.bootstrap.timepicker"]);
-angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.transition", "ui.bootstrap.collapse", "'ui.bootstrap.accordion", "ui.bootstrap.modal"]);
+angular.module("ui.bootstrap", ["ui.bootstrap.tpls", "ui.bootstrap.transition", "ui.bootstrap.collapse", "'ui.bootstrap.accordion", "ui.bootstrap.modal", "ui.bootstrap.alert"]);
 //angular.module("ui.bootstrap.tpls", ["template/accordion/accordion-group.html","template/accordion/accordion.html","template/alert/alert.html","template/carousel/carousel.html","template/carousel/slide.html","template/datepicker/datepicker.html","template/datepicker/popup.html","template/modal/backdrop.html","template/modal/window.html","template/pagination/pager.html","template/pagination/pagination.html","template/tooltip/tooltip-html-unsafe-popup.html","template/tooltip/tooltip-popup.html","template/popover/popover.html","template/progressbar/bar.html","template/progressbar/progress.html","template/rating/rating.html","template/tabs/tab.html","template/tabs/tabset-titles.html","template/tabs/tabset.html","template/timepicker/timepicker.html"]);
-angular.module("ui.bootstrap.tpls", ["template/accordion/accordion-group.html", "template/accordion/accordion.html", "template/modal/backdrop.html","template/modal/window.html"]);
+angular.module("ui.bootstrap.tpls", ["template/accordion/accordion-group.html", "template/accordion/accordion.html", "template/modal/backdrop.html","template/modal/window.html", "template/alert/alert.html"]);
 
 
 angular.module('ui.bootstrap.transition', [])
@@ -331,7 +331,7 @@ angular.module('ui.bootstrap.accordion', ['ui.bootstrap.collapse'])
 });
 
 
-
+// 3.0 done
 angular.module("ui.bootstrap.alert", []).directive('alert', function () {
   return {
     restrict:'EA',
@@ -347,6 +347,8 @@ angular.module("ui.bootstrap.alert", []).directive('alert', function () {
     }
   };
 });
+
+
 
 angular.module('ui.bootstrap.bindHtml', [])
 
@@ -3000,26 +3002,27 @@ angular.module('ui.bootstrap.timepicker', [])
 
 angular.module("template/accordion/accordion-group.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/accordion/accordion-group.html",
-    "<div class=\"panel panel-default\">" +
-    "  <div class=\"panel-heading\">" +
-    "    <h4 class=\"panel-title\">" +
-    "      <a href=\"\" class=\"accordion-toggle\" ng-click=\"isOpen = !isOpen\" accordion-transclude=\"heading\">{{heading}}</a>" +
-    "    </h4>" +
-    "  </div>" +
-    "  <div class=\"panel-collapse\" collapse=\"!isOpen\">" +
-    "          <div class=\"panel-body\" ng-transclude></div>" +
-    "  </div>" +
-    "</div>");
+    "<div class=\"panel panel-default\">\n" +
+    "  <div class=\"panel-heading\">\n" +
+    "    <h4 class=\"panel-title\">\n" +
+    "      <a href=\"\" class=\"accordion-toggle\" ng-click=\"isOpen = !isOpen\" accordion-transclude=\"heading\">{{heading}}</a>\n" +
+    "    </h4>\n" +
+    "  </div>\n" +
+    "  <div class=\"panel-collapse\" collapse=\"!isOpen\">\n" +
+    "          <div class=\"panel-body\" ng-transclude></div>\n" +
+    "  </div>\n" +
+    "</div>\n" +
+    "");
 }]);
 
 angular.module("template/accordion/accordion.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/accordion/accordion.html",
-    "<div class=\"panel-group\" ng-transclude></div>");
+    "<div class=\"panel-group\" ng-transclude></div>\n");
 }]);
 
 angular.module("template/alert/alert.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("template/alert/alert.html",
-    "<div class='alert' ng-class='type && \"alert-\" + type'>\n" +
+    "<div class='alert' ng-class='\"alert-\" + (type || \"warning\")'>\n" +
     "    <button ng-show='closeable' type='button' class='close' ng-click='close()'>&times;</button>\n" +
     "    <div ng-transclude></div>\n" +
     "</div>\n" +
